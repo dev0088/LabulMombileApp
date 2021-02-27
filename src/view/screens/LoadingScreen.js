1390aa89082c33aa20fb0d4ab8c3830f1a14307a
dev0-cup <stannnnislav@yandex.com>
2021-02-27 17:18:55 +0400

    Made progress

--- src/view/screens/LoadingScreen.js
+++ src/view/screens/LoadingScreen.js
@@ -1,13 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { em } from '../common/const';
+import { em, WIDTH } from '../common/const';
 import LinearGradient from 'react-native-linear-gradient';
 
 const LoadingScreen = (props) => {
+  const [percent, setPercent] = useState(0);
+
   useEffect(() => {
-    setTimeout(() => {}, 500);
-  }, []);
+    if (percent < 100) {
+      increasePercent();
+    }
+  }, [percent]);
+
+  function increasePercent() {
+    setTimeout(() => {
+      var newPercent = percent + 1.5;
+      if (newPercent > 100) {
+        newPercent = 100;
+      }
+      setPercent(newPercent);
+    }, 40);
+  }
 
   return (
     <View style={styles.container}>
@@ -18,7 +32,16 @@ const LoadingScreen = (props) => {
         angle={180}
         colors={['#64E4F4', '#4BD8E9']}
         style={styles.linearGradient}>
-        <Text>HABUL</Text>
+        <View style={styles.topHalfContainer}>
+          <Text>Habule Title</Text>
+          <Text>Habule Logo</Text>
+        </View>
+        <View style={styles.bottomHalfContainer}>
+          <Text>Habule Title</Text>
+          <View style={styles.progressContainer}>
+            <View style={[{ width: ((WIDTH - 120 * em) / 100) * percent }, styles.progressBar]} />
+          </View>
+        </View>
       </LinearGradient>
     </View>
   );
@@ -27,8 +50,26 @@ const LoadingScreen = (props) => {
 const styles = {
   container: {
     flex: 1,
+  },
+  topHalfContainer: {
+    flex: 0.5,
+    flexDirection: 'column-reverse',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  bottomHalfContainer: {
+    flex: 0.5,
+    alignItems: 'center',
+  },
+  progressBar: {
+    height: 6 * em,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 3 * em,
+  },
+  progressContainer: {
+    width: WIDTH - 120 * em,
+    height: 6 * em,
+    backgroundColor: '#A4E4F4',
+    borderRadius: 3 * em,
   },
   title: {
     fontSize: 40 * em,
